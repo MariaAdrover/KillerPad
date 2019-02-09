84731aa59ab9fe25cd9151d632d7606f6c3ed21c
pauuu <belcegorx@gmail.com>
2019-02-09 17:44:27 +0100

    dialog funcional
    me siento inutil por estas mierdas de 3 lienas

--- app/src/main/java/com/example/killerpad/MenuFragment.java
+++ app/src/main/java/com/example/killerpad/MenuFragment.java
@@ -1,6 +1,8 @@
 package com.example.killerpad;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -28,38 +30,51 @@ public class MenuFragment extends Fragment {
             public void onClick(View v) {
                 // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                Dialog dialog = new Dialog(getContext());
+
 
 // 2. Chain together various setter methods to set the dialog characteristics
                 builder.setMessage("nazi")
                         .setTitle("pene");
 
+
 // 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
-                AlertDialog dialog = builder.create();
+                //AlertDialog dialog = builder.create();
+                dialog.setContentView(R.layout.dialog_signin);
+
                 dialog.show();
                 //Intent intent = new Intent(getActivity(), PadActivity.class);
                 //startActivity(intent);
             }
         });
 
-
-        //this.buttonInfoDialog = (Button) v.findViewById(R.id.buttonInfo);
-        /*
-        this.buttonInfoDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-// 2. Chain together various setter methods to set the dialog characteristics
-                builder.setMessage("nazi")
-                        .setTitle("pene");
-
-// 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
-                AlertDialog dialog = builder.create();
-            }
-        });*/
-
         return v;
     }
+
+    /*
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_signin, null))
+                // Add action buttons
+                .setPositiveButton("signin Hardcoded", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton("cancelHardcoded", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        LoginDialogFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }*/
 
 }
