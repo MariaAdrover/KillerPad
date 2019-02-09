8ee8046b7be996ed43aadd9f1cf90adf0c32963a
pauuu <belcegorx@gmail.com>
2019-02-09 17:18:26 +0100

    Añadido primer dialog
    Añadido primer dialog prefabricado. El dialog de los misiles no hace nada de momento, será usado mas tarde. De momento solo muestra un dialogo en blanco y ya está. Lo que llama al dialog es el boton que carga el mando. No se pq, al añadir un escuchador a los botones de material design (info, scores, settings), la app peta

--- app/src/main/java/com/example/killerpad/MenuFragment.java
+++ app/src/main/java/com/example/killerpad/MenuFragment.java
@@ -1,5 +1,6 @@
 package com.example.killerpad;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,7 +10,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class MenuFragment extends Fragment {
-    Button goPad;
+    private Button goPad;
+    private Button buttonInfoDialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,10 +26,38 @@ public class MenuFragment extends Fragment {
         this.goPad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), PadActivity.class);
-                startActivity(intent);
+                // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+// 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage("nazi")
+                        .setTitle("pene");
+
+// 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                //Intent intent = new Intent(getActivity(), PadActivity.class);
+                //startActivity(intent);
             }
         });
+
+
+        //this.buttonInfoDialog = (Button) v.findViewById(R.id.buttonInfo);
+        /*
+        this.buttonInfoDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+// 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage("nazi")
+                        .setTitle("pene");
+
+// 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
+                AlertDialog dialog = builder.create();
+            }
+        });*/
 
         return v;
     }
