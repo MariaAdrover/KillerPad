2e7dbcfb7830c28c3c69a091333a2441ca792f05
pauuu <belcegorx@gmail.com>
2019-02-09 18:23:55 +0100

    añadido dialogo ip + puerto
    añadir escuchadores al resto de botones

--- app/src/main/java/com/example/killerpad/MenuFragment.java
+++ app/src/main/java/com/example/killerpad/MenuFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +14,7 @@ import android.widget.Button;
 
 public class MenuFragment extends Fragment {
     private Button goPad;
-    private Button buttonInfoDialog;
+    private FloatingActionButton buttonInfoDialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,53 +29,29 @@ public class MenuFragment extends Fragment {
         this.goPad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 Dialog dialog = new Dialog(getContext());
 
-
-// 2. Chain together various setter methods to set the dialog characteristics
-                builder.setMessage("nazi")
-                        .setTitle("pene");
-
-
-// 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
-                //AlertDialog dialog = builder.create();
                 dialog.setContentView(R.layout.dialog_signin);
 
                 dialog.show();
-                //Intent intent = new Intent(getActivity(), PadActivity.class);
-                //startActivity(intent);
+            }
+        });
+
+        this.buttonInfoDialog = (FloatingActionButton) v.findViewById(R.id.buttonInfo);
+        this.goPad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getContext());
+
+                dialog.setContentView(R.layout.dialog_signin);
+
+                dialog.show();
             }
         });
 
         return v;
     }
 
-    /*
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        // Get the layout inflater
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
-        builder.setView(inflater.inflate(R.layout.dialog_signin, null))
-                // Add action buttons
-                .setPositiveButton("signin Hardcoded", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        // sign in the user ...
-                    }
-                })
-                .setNegativeButton("cancelHardcoded", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        LoginDialogFragment.this.getDialog().cancel();
-                    }
-                });
-        return builder.create();
-    }*/
 
 }
