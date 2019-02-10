0ac936bc5b4933898c1e41822ab857efa5d51f34
pauuu <belcegorx@gmail.com>
2019-02-10 13:32:23 +0100

    acabado el layout del dialog
    Acabado y corregido el layout del dialog. El boton de acceptar lleva al mando (sin nada dentro). Falta añadir una restincion que no permita iniciar el mando sin ninguna ip, puerto ni usr
    
    test

--- app/src/main/java/com/example/killerpad/MenuFragment.java
+++ app/src/main/java/com/example/killerpad/MenuFragment.java
@@ -1,8 +1,6 @@
 package com.example.killerpad;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MenuFragment extends Fragment {
     private Button goPad;
@@ -25,27 +24,42 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_menu, container, false);
 
+        //añade listener en el boton.
         this.goPad = (Button) v.findViewById(R.id.go_to_pad);
+
+        //mostrar dialogo
         this.goPad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Dialog dialog = new Dialog(getContext());
-
-                dialog.setContentView(R.layout.dialog_signin);
-
+                dialog.setContentView(R.layout.dialog_connect);
                 dialog.show();
+
+
+                FloatingActionButton bGo = dialog.findViewById(R.id.fButtonAceptar);
+                bGo.setOnClickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getActivity(), PadActivity.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
 
         this.buttonInfoDialog = (FloatingActionButton) v.findViewById(R.id.buttonInfo);
-        this.goPad.setOnClickListener(new View.OnClickListener() {
+
+        //test
+        this.buttonInfoDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Dialog dialog = new Dialog(getContext());
-
-                dialog.setContentView(R.layout.dialog_signin);
-
+                dialog.setContentView(R.layout.dialog_connect);
                 dialog.show();
+
+                String user = ((EditText) dialog.findViewById(R.id.ip)).getText().toString();
+
             }
         });
 
