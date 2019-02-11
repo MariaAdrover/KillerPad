baa1963b0bf4ed98416931f56b27e95b607ab8e9
pauuu <belcegorx@gmail.com>
2019-02-11 12:25:05 +0100

    Juntadas las tres ramas
    juntado el trabajo de las tres ramas sin haber hecho un merge pq si no hubiese habido conflictos por todo.

--- app/src/main/java/com/example/killerpad/PadActivity.java
+++ app/src/main/java/com/example/killerpad/PadActivity.java
@@ -1,17 +1,43 @@
 package com.example.killerpad;
 
-import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-public class PadActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class PadActivity extends AppCompatActivity implements JoystickView.JoystickListener {
+    private Socket socket = null;
+    private String ip = "192.168.1.5";
+    private int port = 1500;
+    private BufferedReader in;
+    private PrintWriter out;
     private Handler handler;
 
     public Handler getHandler() {
         return this.handler;
+    }
+
+    private void requestConnection() {
+        try {
+            this.socket = new Socket(ip, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
     }
 
     @Override
@@ -19,48 +45,47 @@ public class PadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pad);
 
-        /*permitir conexion en el main??*/
+       //permitir conexion en el main
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build());
 
-        //poner el valor de las variables segun el valor puesto x los putExtra
-        Bundle extras = getIntent().getExtras();
-        String user = extras.getString("user");
-        String ip = extras.getString("ip");
-        int port = extras.getInt("port");
+        //solicitar conexion
+        while (this.socket == null) {
+            this.requestConnection();
+        }
 
         //crear handler
-        this.handler = new Handler(user, ip, port);
+        this.handler = new Handler();
 
-        this.handler.configureConnection();
+        //configurar handler
+        this.handler.setHandler(this.socket, this.ip, this.port);
 
         Thread t = new Thread(this.handler);
         t.start();
 
-        //crear los fragments del mando: joystick, buttons y panelInfo(board)
+        //Enviar mensaje de prueba desde handler
+        this.handler.sendMessage("handler: holi desde el kk mando");
+
+        //crear los fragments
         FragmentManager fm = getSupportFragmentManager();
 
-        Fragment boardFragment;
-
-        boardFragment = fm.findFragmentById(R.id.board_container);
-        if (boardFragment == null) {
-            boardFragment = new BoardFragment();
+        Fragment board_fragment = fm.findFragmentById(R.id.board_container);
+        if (board_fragment == null) {
+            board_fragment = new BoardFragment();
             fm.beginTransaction()
-                    .add(R.id.board_container, boardFragment) .commit();
+                    .add(R.id.board_container, board_fragment) .commit();
         }
-
-        Fragment joystickFragment = fm.findFragmentById(R.id.joystick_container);
-        if (joystickFragment == null) {
-            joystickFragment = new JoystickFragment();
+        Fragment joystick_fragment = fm.findFragmentById(R.id.joystick_container);
+        if (joystick_fragment == null) {
+            joystick_fragment = new JoystickFragment();
             fm.beginTransaction()
-                    .add(R.id.joystick_container, joystickFragment) .commit();
+                    .add(R.id.joystick_container, joystick_fragment) .commit();
         }
-
-        Fragment buttonsFragment = fm.findFragmentById(R.id.buttons_container);
-        if (buttonsFragment == null) {
-            buttonsFragment = new ButtonsFragment();
+        Fragment buttons_fragment = fm.findFragmentById(R.id.buttons_container);
+        if (buttons_fragment == null) {
+            buttons_fragment = new ButtonsFragment();
             fm.beginTransaction()
-                    .add(R.id.buttons_container, buttonsFragment) .commit();
+                    .add(R.id.buttons_container, buttons_fragment) .commit();
         }
     }
 
@@ -79,5 +104,16 @@ public class PadActivity extends AppCompatActivity {
         //ActionBar actionBar = getActionBar();
         //actionBar.hide();
         getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onJoystickMoved(float xPercent, float yPercent, int source) {
+      //  Log.d("move","X: "+xPercent+" Y: "+yPercent);
+    }
+
+    @Override
+    public void directionChanged(String direction) {
+        Log.d("move",direction);
+        handler.sendMessage(direction);
     }
 }
