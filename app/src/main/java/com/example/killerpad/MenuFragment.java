103e7b013df2e88744749aa03f23ac8849fe84d4
pauuu <belcegorx@gmail.com>
2019-03-03 22:36:49 +0100

    it ain't much, but it's honest work

--- app/src/main/java/com/example/killerpad/MenuFragment.java
+++ app/src/main/java/com/example/killerpad/MenuFragment.java
@@ -3,19 +3,16 @@ package com.example.killerpad;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-
-import static android.support.v4.content.ContextCompat.getSystemService;
+import android.widget.TextView;
 
 
 /**
@@ -26,7 +23,12 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
 
     private Bundle sis;
     private Button goPad;
-    private FloatingActionButton buttonInfoDialog;
+    private Button buttonInfoDialog;
+
+    //Dialogs
+    private Dialog colorDialog;
+    private Dialog configurationDialog;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,14 +39,17 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        // carga el layout
         View v = inflater.inflate(R.layout.fragment_menu, container, false);
 
         Vibrator vib = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
-        vib.vibrate(500);
+        vib.vibrate(400);
 
-
+        // para comenzar nueva partida => muestra el dialogo de conifguracion
         this.goPad = (Button) v.findViewById(R.id.go_to_pad);
-        this.buttonInfoDialog = (FloatingActionButton) v.findViewById(R.id.buttonInfo);
+
+        // para elegit el color de la nave
+        this.buttonInfoDialog = (Button) v.findViewById(R.id.buttonInfo);
 
         //añade listener en el boton.
         this.goPad.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +65,7 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
         this.buttonInfoDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showInfoDialog();
+                showColorPickerDialog();
             }
         });
 
@@ -69,27 +74,7 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
 
     //************** myMethods ****************
 
-    private void showInfoDialog() {
-        FloatingActionButton[] a;
-
-
-        final Dialog dialog = new Dialog(getContext());
-        dialog.setContentView(R.layout.dialog_color_picker); //añade ñayout al dialogo
-        dialog.show();  //muestra el dialogo
-
-
-    }
-
-    private void loadConfig(String key, EditText et) {
-        et.setText(((MenuActivity) getActivity()).loadPreference(key));
-    }
-
-    private void saveConfig(String key, String value) {
-        ((MenuActivity) getActivity()).savePreferences(key, value);
-    }
-
-    private void showConfigDialog() {
-
+    private void loadConfigurationDialoog() {
         FloatingActionButton bAceptar;
         FloatingActionButton bCancelar;
 
@@ -98,43 +83,117 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
         EditText etPort;
 
         // para que pueda acceder al metodo ik
-        final Dialog dialog = new Dialog(getContext());
-        dialog.setContentView(R.layout.dialog_connect); //añade ñayout al dialogo
-        dialog.show();  //muestra el dialogo
-        
-
-
-        bAceptar = dialog.findViewById(R.id.fButtonAceptar);
-        bCancelar = dialog.findViewById(R.id.fButtonCancelar);
+        bAceptar = this.configurationDialog.findViewById(R.id.fButtonAceptar);
+        bCancelar = this.configurationDialog.findViewById(R.id.fButtonCancelar);
 
         // editText de cada campo
-        etUsername = dialog.findViewById(R.id.username);
-        etIp = dialog.findViewById(R.id.ip);
-        etPort = dialog.findViewById(R.id.puerto);
+        etUsername = this.configurationDialog.findViewById(R.id.username);
+        etIp = this.configurationDialog.findViewById(R.id.ip);
+        etPort = this.configurationDialog.findViewById(R.id.puerto);
 
         // carga las configuraciones con las shared preferences
         loadConfig("user", etUsername);
         loadConfig("ip", etIp);
         loadConfig("port", etPort);
-        bAceptar.setOnClickListener(this);
 
-        //EVENTO: al pular boton aceptar: configurar ursName, ip, puerto
-        bAceptar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        bAceptar.setOnClickListener(this);
+        bCancelar.setOnClickListener(this);
+
+    }
+
+    private void loadConfig(String key, EditText et) {
+        et.setText(((MenuActivity) getActivity()).loadPreference(key));
+    }
+
+    private void showColorPickerDialog() {
+
+        this.colorDialog = new Dialog(this.getContext());
+        this.colorDialog.setContentView(R.layout.dialog_color_picker);
+        this.addColorListeners();
+        this.colorDialog.show();  //muestra el dialogo
+
+    }
+
+    private void addColorListeners() {
+
+        FloatingActionButton[] colorButton = new FloatingActionButton[10]; //test
+
+        Button colorOrangeButton;
+        Button colorGreenButton;
+        Button colorPinkButton;
+        Button acceptButton;
+
+
+        colorOrangeButton = this.colorDialog.findViewById(R.id.orangeButton);
+        colorGreenButton = this.colorDialog.findViewById(R.id.greenButton);
+        colorPinkButton = this.colorDialog.findViewById(R.id.pinkButton);
+        acceptButton = this.colorDialog.findViewById(R.id.accept);
+
+
+        colorOrangeButton.setOnClickListener(this);
+        colorGreenButton.setOnClickListener(this);
+        colorPinkButton.setOnClickListener(this);
+        acceptButton.setOnClickListener(this);
+
+    }
+
+    private void showConfigDialog() {
+
+        this.configurationDialog = new Dialog(this.getContext());
+        this.configurationDialog.setContentView(R.layout.dialog_configuration);
+        this.loadConfigurationDialoog();
+        this.configurationDialog.show();
+
+    }
+
+    private void saveConfig(String key, String value) {
+        ((MenuActivity) getActivity()).savePreferences(key, value);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            // dialoog de los colores
+            case R.id.orangeButton:
+                this.saveConfig("color", "#333456");
+                ((TextView) this.colorDialog.findViewById(R.id.colorTest)).setText("naranja");
+                break;
+
+            case R.id.pinkButton:
+                this.saveConfig("color", "ff0000");
+                ((TextView) this.colorDialog.findViewById(R.id.colorTest)).setText("rojo");
+                break;
+
+            case R.id.greenButton:
+                this.saveConfig("color", "#33cc33");
+                ((TextView) this.colorDialog.findViewById(R.id.colorTest)).setText("verde");
+                break;
+
+            // dialogo para empezar nueva partida
+            case R.id.accept:
+                this.colorDialog.cancel();
+                break;
+
+            case R.id.fButtonAceptar:
                 Intent intent;
                 String user;
                 String ip;
                 int port;
 
+                // cierra el dialogo
+                this.configurationDialog.cancel();
+
+                // crea un nuevo intent
                 intent = new Intent(getActivity(), PadActivity.class);
 
-                //  coje del dialogo el obj del editText e indicamos q es un editText (parse). Luego pillamos el texto de dentro y lo pasamos a string
-                user = ((EditText) dialog.findViewById(R.id.username)).getText().toString();
-                ip = ((EditText) dialog.findViewById(R.id.ip)).getText().toString();
+                //  coge del dialogo el obj del editText e indicamos q es un editText (parse). Luego pillamos el texto de dentro y lo pasamos a string
+                user = ((EditText) this.configurationDialog.findViewById(R.id.username)).getText().toString();
+                ip = ((EditText) this.configurationDialog.findViewById(R.id.ip)).getText().toString();
 
                 //matadme por favor...
-                port = Integer.parseInt(((EditText) dialog.findViewById(R.id.puerto)).getText().toString());
+                port = Integer.parseInt(((EditText) this.configurationDialog.findViewById(R.id.puerto)).getText().toString());
 
                 // se las pasa por gracias al put extra
                 intent.putExtra("user", user);
@@ -146,37 +205,22 @@ public class MenuFragment extends Fragment implements View.OnClickListener {
                 saveConfig("ip", ip);
                 saveConfig("port", String.valueOf(port));
 
-                dialog.cancel();
+
+                // cierra el dialogo
+                this.configurationDialog.cancel();
+
+                // inicia la actividad del pad
                 startActivity(intent);
+
+                // acaba la menu activity
                 getActivity().finish();
-
-            }
-        });
-
-        //EVENTO: al pusltar boton canclear: cerrar dialogo
-        bCancelar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.cancel();
-            }
-        });
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        String s = v.getResources().getResourceEntryName(v.getId());
-
-        switch (s){
-            case "rojo":
                 break;
 
-            case "azul":
-                break;
-
-            case "verde":
+            case R.id.fButtonCancelar:
+                this.colorDialog.cancel();
                 break;
 
         }
     }
+
 }
